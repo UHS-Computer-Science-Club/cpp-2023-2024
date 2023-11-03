6b5b3d9a7e0f2ad60cf7de356bf51cfb6197ebac
powersagitar <114868133+powersagitar@users.noreply.github.com>
2023-11-03 17:21:31 -0400

    Add explanation

--- 11-03-2023/macos_setup_oneliner.sh
+++ 11-03-2023/macos_setup_oneliner.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 # installs xcode command line tools
 xcode-select --install
@@ -6,8 +6,11 @@ xcode-select --install
 # installs homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# installs cmake 
+# installs cmake
 brew install cmake
 
 # installs ninja
 brew install ninja
+
+echo 'If the install script prompts that brew is not found, close the terminal, start a new session, and run `brew install cmake ninja`'
+echo "The reason why brew will not be found when you first install it is that the PATH environment variable hasn't been reloaded after being modified. Starting a new shell session will reload the environment variable therefore brew will work from then on."
